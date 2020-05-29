897df6a2ac7f91c5974428cb92648f2946e9ea5f
Bryant Luk <bryant.luk@bryantluk.com>
2020-05-29 09:26:18 -0500

    Add bootstrap.zsh

---
+++ bootstrap.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+xcode-select -p >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Installing Xcode CLI Tools"
+  xcode-select --install
+fi
+
+if [[ ! -x /usr/local/bin/brew ]]; then
+  echo "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [[ ! -x /usr/local/bin/ansible ]]; then
+  echo "Installing Ansible"
+  pip install ansible
+fi
+
+ansible-galaxy install -r requirements.yml
+
+ansible-playbook -i inventory main.yml
